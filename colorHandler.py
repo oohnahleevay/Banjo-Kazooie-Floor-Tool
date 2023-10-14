b45ba5dd73d876e9b0575742d3f99ced487edd78
Unalive <147900094+oohnahleevay@users.noreply.github.com>
2023-10-14 00:44:12 -0400

    Actually upload the files this time
    Ooer how do I github please tell me how to do libraries

---
+++ colorHandler.py
@@ -0,0 +1,116 @@
+import math
+### I'll need these eventually to draw the textures ###
+# import numpy as np
+# from PIL import Image as im
+
+texture = []
+colorFormat = {
+    1:"CI4",
+    2:"CI8",
+    4:"RGBA5551",
+    8:"RGBA8888"
+}
+
+#TODO: Move class defs to their own file
+class Texture():
+    def __init__(self, Offset, nextOffset, Type, X, Y):
+        self.Offset = Offset
+        self.nextOffset = nextOffset
+        self.type = colorFormat[Type]
+        self.colorBytes = Type/2
+        self.x_dim = X
+        self.y_dim = Y
+
+class Pixel():
+    def __init__(self, positionAbs, positionX, positionY, R, G, B, A):
+        self.positionAbs = positionAbs
+        self.positionX = positionX
+        self.positionY = positionY
+        self.red = R
+        self.green = G
+        self.blue = B
+        self.alpha = A
+
+#This lives here now
+def isModelFile(file):
+    isModelFile = int.from_bytes(file.read(4), "big")
+    if (isModelFile != 0xB):
+        print("Not a model file!")
+        return False
+
+#Gets 5-bit color and shifts it into 8-bit color
+def getRGBA(RGBA, splitRGBA):
+    splitRGBA[0] = (RGBA & 0xF800) / 0x100
+    splitRGBA[1] = (RGBA & 0x7C0) / 0x8
+    splitRGBA[2] = (RGBA & 0x3E) * 0x4
+    splitRGBA[3] = (RGBA & 0x1) * 0xFF
+    return splitRGBA
+
+#this is a monster
+#TODO: Comment this
+def getTextureInfo(fileName):
+    with open(fileName, "rb") as file:
+        if (isModelFile(file) == "False"):
+            return
+        file.seek(0x8, 0)
+        textureSetupOffset = int.from_bytes(file.read(2), "big")
+        if (textureSetupOffset == 0):
+            print("This model has no textures!")
+            return
+        file.seek(textureSetupOffset,0)
+        maxBytes = int.from_bytes(file.read(4), "big")
+        textureCount = int.from_bytes(file.read(2), "big")
+        file.seek(2, 1)
+        for tex in range(textureCount):
+            BufferOffset = int.from_bytes(file.read(4),"big")
+            BufferType = int.from_bytes(file.read(2),"big")
+            file.seek(2,1)
+            BufferX = int.from_bytes(file.read(1),"big")
+            BufferY = int.from_bytes(file.read(1),"big")
+            file.seek(6,1)
+            if (tex + 1 == textureCount):
+                BufferNextOffset = maxBytes
+            else:
+                BufferNextOffset = int.from_bytes(file.read(4),"big")
+                file.seek(-4,1)
+            texture.append(Texture(BufferOffset, BufferNextOffset, BufferType, BufferX, BufferY))
+        for tx in texture:
+            tx.pixel = []
+            length = tx.nextOffset - tx.Offset
+            print(tx.type)
+            if (tx.type == "CI4"):
+                tx.pixelCount = (length - 32) / tx.colorBytes
+                tx.palette = []
+                for color in range(16):
+                    tx.palette.append(int.from_bytes(file.read(2),"big"))
+            elif (tx.type == "CI8"):
+                for color in range(16):
+                    tx.palette.append(int.from_bytes(file.read(2),"big"))
+            else:
+                tx.pixelCount = length / tx.colorBytes
+            for pix in range(int(tx.pixelCount)):
+                BufferPosY = math.floor(pix / 32)
+                BufferPosX = math.fmod(pix, 32)
+                splitRGBA = [0,0,0,0]
+                if (tx.type == "CI4"):
+                    readByte = int(math.fmod(pix, 2))
+                    if (readByte == 0):
+                        byte = int.from_bytes(file.read(1),"big")
+                    nibble = [0,0]
+                    nibble[0] = int((byte & 0xF0) / 0x10)
+                    nibble[1] = (byte & 0xF)
+                    RGBA = tx.palette[nibble[readByte]]
+                    getRGBA(RGBA, splitRGBA)
+                elif (tx.type == "CI8"):
+                    index = int.from_bytes(file.read(1),"big")
+                    RGBA = tx.palette[index]
+                    getRGBA(RGBA, splitRGBA)
+                elif (tx.type == "RGBA5551"):
+                    RGBA = int.from_bytes(file.read(2),"big")
+                    getRGBA(RGBA, splitRGBA)
+                elif (tx.type == "RGB8888"):
+                    splitRGBA[0] = int.from_bytes(file.read(1), "big")
+                    splitRGBA[1] = int.from_bytes(file.read(1), "big")
+                    splitRGBA[2] = int.from_bytes(file.read(1), "big")
+                    splitRGBA[3] = int.from_bytes(file.read(1), "big")
+                tx.pixel.append(Pixel(pix, BufferPosX, BufferPosY, splitRGBA[0], splitRGBA[1], splitRGBA[2], splitRGBA[3]))
---
+++ main.py
@@ -0,0 +1,71 @@
+from colorHandler import getTextureInfo, isModelFile, texture
+
+collisionTri = []
+outputFileName = "output.txt" #lol
+
+#TODO: Move class defs to separate file
+class Tri():
+    def __init__(self, VertexA, VertexB, VertexC, Flags):
+        self.VertexA = VertexA
+        self.VertexB = VertexB
+        self.VertexC = VertexC
+        self.Flags = []
+        self.trueFlags = []
+
+        #turn the bitfield into a list for ease of access
+        for i in range(0x20):
+            self.Flags.append(bool(Flags & (2 ** i) != 0))
+
+        #might be useful later idk
+        for index, flag in enumerate(self.Flags):
+            if (flag == True):
+                index = hex(index)
+                self.trueFlags.append(index)
+
+def getCollisionTris(fileName):
+    with open(fileName, "rb") as file:
+        if (isModelFile(file) == "False"):
+            return
+        file.seek(0x1c, 0)
+        collisionOffset = int.from_bytes(file.read(4),"big")
+        if(collisionOffset == 0):
+            print("This file has no collisions!")
+            return
+        file.seek(collisionOffset,0)
+        file.seek(16, 1)
+
+        #count the unknown block so I can skip it
+        unknownSize = int.from_bytes(file.read(2),"big") * 4
+        file.seek(2, 1)
+
+        triCount = int.from_bytes(file.read(2),"big")
+        file.seek(2,1)
+        file.seek(unknownSize,1)
+
+        for tri in range(triCount):
+            BufferA = int.from_bytes(file.read(2),"big")
+            BufferB = int.from_bytes(file.read(2),"big")
+            BufferC = int.from_bytes(file.read(2),"big")
+            file.seek(2,1)
+            FlagBuffer = int.from_bytes(file.read(4),"big")
+            collisionTri.append(Tri(BufferA, BufferB, BufferC, FlagBuffer))
+        print("success")
+
+#print this shit to a file for now
+def printToFile(fileName):
+        with open(fileName, "w") as file:
+            tri_count = 0
+            for tri in collisionTri:
+                file.write("Tri #{}: \n\t Vertex IDs: {}, {}, {} \n\t Flags: {} \n".format(tri_count, hex(tri.VertexA), hex(tri.VertexB), hex(tri.VertexC), tri.trueFlags))
+                tri_count += 1
+            tex_count = 0
+            for tex in texture:
+                file.write("Texture #{}: \n\t Color Type: {}: \n".format(tex_count, tex.type))
+                tex_count += 1
+                for pix in tex.pixel:
+                    file.write("\t Pixel ({}, {}): {}, {}, {}, {} \n".format(pix.positionX, pix.positionY, pix.red, pix.green, pix.blue, pix.alpha))
+
+fileName = input("fileName: ")
+getTextureInfo(fileName)
+getCollisionTris(fileName)
+printToFile(outputFileName)
